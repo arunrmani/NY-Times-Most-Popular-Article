4e6a01c032b29521ba27b31a7a386faf3361ea03
aicenter-ios <arunrmani@aicenter.ae>
2021-12-22 13:22:43 +0400

    Initial Commit

---
+++ NY Articles/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NY Articles
+//
+//  Created by Safe City Mac 001 on 22/12/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ NY ArticlesTests/NY_ArticlesTests.swift
@@ -0,0 +1,33 @@
+//
+//  NY_ArticlesTests.swift
+//  NY ArticlesTests
+//
+//  Created by Safe City Mac 001 on 22/12/2021.
+//
+
+import XCTest
+@testable import NY_Articles
+
+class NY_ArticlesTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
